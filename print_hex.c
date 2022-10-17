eec2427713420d11849ed46d5e6fdf55d4aeacb7
John Mark <ojidemark@gmail.com>
2022-10-17 01:43:01 -0700

    My printf commit

--- print_hex.c
+++ print_hex.c
@@ -1,9 +1,8 @@
-
-include "main.h"
+#include "main.h"
 /**
  * print_hex - prints a decimal in hexadecimal
  * @arguments: input string
- * @buf: buffer pointer
+ *@buf: buffer pointer
  * @ibuf: index for buffer pointer
  * Return: number of chars printed
  */
